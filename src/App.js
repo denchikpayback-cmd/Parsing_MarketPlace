df6009a7db6bb54fb1677257b852ccaf254d5961
VIEWER <denisviewer@mail.ru>
2026-08-03 09:44:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,23 @@
-import React, { useState, useRef } from "react";
-import "./App.css";
-import Card from "./components/Card";
-
+import Cards from ".//components/Card.jsx";
+import "./App.css"
 function App() {
-  const inputRef = useRef(null)
-  const [city, setCity] = useState("")
-  const [weather, setWeather] = useState()
-  const [load, setLoad] = useState()
-  const [err, setErr] = useState()
-  const [flag, setFlag] = useState(false)
-
-  async function weat(cityName) {
-    try {
-      const response = await fetch(`https://wttr.in/${cityName}?format=j1&lang=ru`)
-      const data = await response.json()
-      console.log(data)
-
-      const foundCity = data?.nearest_area?.[0]?.areaName?.[0]?.value
-      console.log("Найденный город:", foundCity)
-
-      if (foundCity && foundCity.toLowerCase() === cityName.toLowerCase()) {
-        console.log("Город найден:", cityName)
-        setWeather(data)
-        setCity(cityName)
-        setErr("")
-      } else {
-        console.log("Город не найден, показан:", foundCity)
-        setErr("Город не найден")
-        setWeather(null)
-        setFlag(false)
-      }
-    } catch (err1) {
-      console.log(err1)
-      setErr("Ошибка загрузки")
-    }
-  }
+  
   return (
-    <div className="App">
-      <h1>🌤️ Погода</h1>
-
-      <div className="search-row">
-        <input type="text" ref={inputRef} placeholder="Введите город..." />
-        <button onClick={() => {
-          const cityName = inputRef.current.value
-          setCity(cityName)
-          weat(cityName)
-          setFlag(true)
-        }}>Найти</button>
+    <div className="root">
+      <p>Введите товар который хотите найти</p>
+      <input className="search_row"></input>
+      <button>Найти товар</button>
+      <div className="scrol_product">
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
       </div>
-
-      {err && <p className="error">{err}</p>}
-
-      {flag && weather ? (
-        <Card city={city} data = {weather}/>
-      ) : (
-        <div className="desc">В каком городе хотите узнать погоду</div>
-      )}
     </div>
+    
   );
 }
 
